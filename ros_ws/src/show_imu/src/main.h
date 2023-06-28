8350aeb8786cd15be79d56cd4553232731bb9010
Asgard-Tim <XUJINGHUA03@126.com>
2023-06-28 18:50:08 +0800

    version-1

---
+++ ros_ws/src/show_imu/src/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
+
+#endif
---
+++ ros_ws/src/show_map/src/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <cstring>
+#include <thread>
+#include <opencv2/opencv.hpp>
+#include "ros/ros.h"
+#include "nav_msgs/OccupancyGrid.h"
+
+
+#endif
