8091cc6f21abb5e7d18aa1fd026c46cb4f87421f
Asgard-Tim <XUJINGHUA03@126.com>
2023-07-01 01:11:25 +0800

    final-system-without-task4

---
+++ project/catkin_ws/build/pointcloud_reconstruction/CMakeFiles/reconstruction_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reconstruction_node.dir/src/reconstruction_node.cpp.o"
+  "/home/ubuntu/project/catkin_ws/devel/lib/pointcloud_reconstruction/reconstruction_node.pdb"
+  "/home/ubuntu/project/catkin_ws/devel/lib/pointcloud_reconstruction/reconstruction_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reconstruction_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
